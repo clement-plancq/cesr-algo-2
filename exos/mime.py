2261bb5eab66055010934d7ebb87197d4d2ff117
clement-plancq <clement.plancq@univ-tours.fr>
2024-01-29 09:33:37 +0100

    correction type mime explicitée

--- exos/mime.py
+++ exos/mime.py
@@ -16,7 +16,11 @@ q = int(input())  # Number Q of file names to be analyzed.
 for i in range(n):
     # ext: file extension
     # mime: MIME type
-    ext, mime = input().split()
+    chops = input().split()
+    ext = chops[0]
+    mime = chops[1]
+    # ou en raccourci
+    #ext, mime = input().split()
     mime_dico[ext.lower()] = mime
     
 #print(repr(mime_dico), file=sys.stderr)
@@ -26,7 +30,17 @@ for i in range(q):
     fname = input()  # One file name per line.
     chops = fname.split('.') # on divise le nom de fichier en plusieurs parties
     # on ne garde que la dernière partie (l’extension)
-    ext = chops[-1].lower() if len(chops) > 1 else None 
+    if len(chops) > 1:
+        ext = chops[-1].lower()
+    else:
+        ext = None
+    # ou en raccourci
+    #ext = chops[-1].lower() if len(chops) > 1 else None 
+    if ext in mime_dico:
+        print(mime_dico(ext))
+    else:
+        print("UNKNOWN")
+    # ou en raccourci
     # utilisation de la fonction `get` qui permet de renvoyer une valeur par defaut
     # si la clé n'existe pas
-    print(mime_dico.get(ext, "UNKNOWN"))
+    #print(mime_dico.get(ext, "UNKNOWN"))
